e34d2639eb56ba6fecbc9bbc241ad5e97f32d702
unknown <joao75559@gmail.com>
2024-03-11 09:39:14 -0300

    primeiro commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { HeaderComponent } from './components/header/header.component';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+export const routes: Routes = [
+{
+  path: "",
+  component: HomeComponent 
+  }
+
+
+];
+
